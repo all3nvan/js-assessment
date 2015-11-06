ee729f7d2b7e9a35ab938f8119165446bdde345f
Allen Van <all3nvan@gmail.com>
2015-11-05 20:58:41 -0600

    prepend, curtail, concat, count functions

--- app/arrays.js
+++ app/arrays.js
@@ -49,15 +49,21 @@ define(function() {
     },
 
     prepend : function(arr, item) {
-
+      arr.unshift(item);
+      return arr;
     },
 
     curtail : function(arr) {
-
+      arr.shift();
+      return arr;
     },
 
     concat : function(arr1, arr2) {
+      for (var i = 0; i < arr2.length; i++) {
+        arr1.push(arr2[i]);
+      }
 
+      return arr1;
     },
 
     insert : function(arr, item, index) {
@@ -65,7 +71,15 @@ define(function() {
     },
 
     count : function(arr, item) {
+      var count = 0;
 
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === item) {
+          count++;
+        }
+      }
+
+      return count;
     },
 
     duplicates : function(arr) {
