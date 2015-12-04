1436a2b2fc9d27faf52d7282d5b7ac773a5096d9
Allen Van <all3nvan@gmail.com>
2015-12-04 00:43:51 -0600

    listFiles

--- app/recursion.js
+++ app/recursion.js
@@ -3,7 +3,20 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     listFiles: function(data, dirName) {
+      var files = [];
 
+      for (var i = 0; i < data.files.length; i++) {
+        if (typeof(data.files[i]) === 'string') {
+          files.push(data.files[i]);
+        } else {
+          var nestedFiles = this.listFiles(data.files[i]);
+          for (var j = 0; j < nestedFiles.length; j++) {
+            files.push(nestedFiles[j]);
+          }
+        }
+      }
+
+      return files;
     },
 
     permute: function(arr) {
