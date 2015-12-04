f66c502c6d73d78e0805d2fbe1b2870124ae9ee4
Allen Van <all3nvan@gmail.com>
2015-12-04 00:55:02 -0600

    empty cancel function

--- app/count.js
+++ app/count.js
@@ -3,6 +3,9 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+      return {
+        cancel : function () {}
+      }
     }
   };
 });
